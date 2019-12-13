61712bb430c3abbdfbba160e41bafb600e638075
Philippe Correia <a--@Philippes-MacBook-Pro.local>
2019-12-13 14:28:20 +0100

    modif 2

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -14,19 +14,20 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else if (n < 0)
+	unsigned int nb;
+
+	nb = n;
+	if (nb < 0)
 	{
 		write(fd, "-", 1);
-		n = -n;
-		ft_putnbr_fd(n, fd);
+		nb *= -1;
+		ft_putnbr_fd(nb, fd);
 	}
-	if (n >= 10)
+	else if (nb >= 10)
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd((n % 10 + '0'), fd);
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putchar_fd((nb % 10 + '0'), fd);
 	}
 	else
-		ft_putchar_fd((n + '0'), fd);
+		ft_putchar_fd((nb + '0'), fd);
 }
